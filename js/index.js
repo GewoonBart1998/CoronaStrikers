f805321db9fc7599f11d1a3458aebd030bad355b
GewoonBart1998 <s1115174@student.hsleiden.nl>
2020-06-20 10:22:18 +0200

    bewegen enemys

--- js/index.js
+++ js/index.js
@@ -1,34 +1,38 @@
 
-
 function spawnEnemys(){
-  // spawnlocation camera 0 0 0
-  setTimeout(function() {
-    var box = document.createElement("a-box");
-    box.setAttribute("position", "6 1 0");
-    document.querySelector("#scene").appendChild(box);
-  }, 1000);
+  var i;
+  for (i = 0; i < 8; i++){
+    // spawnlocation camera 0 0 0
 
-  setTimeout(function() {
-    var box = document.createElement("a-box");
-    box.setAttribute("position", "2 1 6");
-    document.querySelector("#scene").appendChild(box);
-  }, 3000);
+    setTimeout(function() {
+      let nummer1 = generateSpawnLocation(-10, 10);
+      let nummer2 = generateSpawnLocation(-10, 10);
+      let enemys = nummer1.toString() + " 1 " + nummer2.toString()
+      let box = document.createElement("a-box");
+      let startX = 0;
+      let startZ = 0;
 
-  setTimeout(function() {
-    var box = document.createElement("a-box");
-    box.setAttribute("position", "3 1 -4");
-    document.querySelector("#scene").appendChild(box);
-  }, 5000);
+      box.setAttribute("position", enemys);
+      document.querySelector("#scene").appendChild(box);
 
-  setTimeout(function() {
-    var box = document.createElement("a-box");
-    box.setAttribute("position", "5 1 5");
-    document.querySelector("#scene").appendChild(box);
-  }, 5000);
 
-  setTimeout(function() {
-    var box = document.createElement("a-box");
-    box.setAttribute("position", "-6 1 -4");
-    document.querySelector("#scene").appendChild(box);
-  }, 5500);
+      let afstand =  Math.sqrt(
+        Math.pow(startX - nummer1, 2) + Math.pow(startZ - nummer2 , 2)
+      );
+      let duration = afstand * 777;
+      console.log(duration);
+      box.setAttribute("animation",  'property: position; to: 0 1 0; dur: ' + duration +";");
+
+
+
+    }, generateSpawnTimer(5000, 10000));
+  }
+}
+
+function generateSpawnLocation(min, max) {
+  return Math.floor(min + Math.random()*(max + 1 - min))
+}
+
+function generateSpawnTimer(min, max) {
+  return Math.floor(min + Math.random()*(max + 1 - min))
 }
