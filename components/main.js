4e8f5d867c300b75d1305b2ece53fa202fc1a597
Matthijs <matthijsvaneijk@gmail.com>
2020-06-23 21:03:14 +0200

    fix

--- components/main.js
+++ components/main.js
@@ -1,7 +1,44 @@
 window.onload = function(){
-  setSpeed();
-  createSpawner();
-  speechRecognitionIsSupported();
+  let options = document.getElementsByClassName("js--option");
+  let cursor = document.getElementById("js--cursor");
+  let score = document.getElementById("js--score");
+  let menu = document.getElementById("menuScene");
+  let game = document.getElementById("gameScene");
+  let hearts = document.getElementById("js--hearts");
+  let voice = document.getElementById("js--voice-plane");
+  let uitleg = document.getElementById("uitlegScene");
+
+  for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', function(evt) {
+      if (options[i].attributes[1].value === "Start") {
+        nextScene("start");
+      } else if (options[i].attributes[1].value === "Uitleg") {
+        nextScene("uitleg");
+      }
+    });
+  }
+
+  function nextScene(str) {
+    switch (str) {
+      case "start":
+        cursor.setAttribute("raycaster", "objects: js--person");
+        score.setAttribute("visible", "true");
+        menu.setAttribute("visible", "false");
+        game.setAttribute("visible", "true");
+        hearts.setAttribute("visible", "true");
+        voice.setAttribute("visible", "true");
+        setSpeed();
+        createSpawner();
+        speechRecognitionIsSupported();
+        break;
+      case "uitleg":
+        menu.setAttribute("visible", "false");
+        uitleg.setAttribute("visible", "true");
+
+        break;
+      default:
+    }
+  }
 };
 
 function setSpeed(){
@@ -12,52 +49,4 @@ function setSpeed(){
 }
 
 
-/*window.onload = function(){
-  let options = document.getElementsByClassName("js--option");
-  let cursor = document.getElementById("js--cursor");
-  let score = document.getElementById("js--score");
-  let menu = document.getElementById("menuScene");
-  let game = document.getElementById("gameScene");
-  let hearts = document.getElementById("js--hearts");
-  let voice = document.getElementById("js--voice-plane");
-  let uitleg = document.getElementById("uitlegScene");
 
-    for (let i = 0; i < options.length; i++) {
-        options[i].addEventListener('click', function(evt) {
-          if (options[i].attributes[1].value === "Start") {
-            nextScene("start");
-          } else if (options[i].attributes[1].value === "Uitleg") {
-            nextScene("uitleg");
-          } else if (options[i].attributes[1].value === "Tutorial") {
-            nextScene("tutorial");
-          }
-
-      });
-    }
-
-    function nextScene(str) {
-      switch (str) {
-        case "start":
-          cursor.setAttribute("raycaster", "objects: js--person");
-          score.setAttribute("visible", "true");
-          menu.setAttribute("visible", "false");
-          game.setAttribute("visible", "true");
-          hearts.setAttribute("visible", "true");
-          voice.setAttribute("visible", "true");
-          attachFollowToFollower(getFollowers());
-          observeAttributesOfEntity(getFollowers());
-          interactWithPeople(getPeople());
-          speechRecognitionIsSupported();
-          setSpeed();
-          createSpawner();
-          speechRecognitionIsSupported();
-          break;
-        case "uitleg":
-          menu.setAttribute("visible", "false");
-          uitleg.setAttribute("visible", "true");
-
-          break;
-        default:
-      }
-    }
-}; */
